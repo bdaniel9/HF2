ced43097f288db6233b93e517d3a6b855c36375b
Daniel Bochis <bochisdaniel3@gmail.com>
2022-11-04 21:00:08 +0100

    Külön osztály létrehozása, switch használata

--- src/main/java/hu/nye/progtech/helloworld/Main.java
+++ src/main/java/hu/nye/progtech/helloworld/Main.java
@@ -4,22 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String nev;
-        String nyelv;
+        Create create = new Create();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Add meg a neved: ");
-        nev = scanner.nextLine();
-
-        System.out.println("Add meg a nyelvet: (hun,eng,haw,ger)");
-        nyelv = scanner.nextLine();
-
-        if (nyelv.equals("hun")) System.out.println("Szia "+nev+"!");
-        else if (nyelv.equals("eng")) System.out.println("Hello, "+nev+"!");
-        else if (nyelv.equals("haw")) System.out.println("Aloha, "+nev+"!");
-        else if (nyelv.equals("ger")) System.out.println("Hallo, "+nev+"!");
-        else{
-            System.out.println("Ismeretlen nyelv.\nMagyarul: Szia "+nev+"!");
-        }
+        create.bekeres();
+        create.kiiratas();
     }
 }
